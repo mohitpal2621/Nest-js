2fa67459098ccc0cf03a000d9307beb5b7f41df9
mohitpal2621 <mohitpal2621@gmail.com>
2025-10-27 05:20:39 +0530

    Initial Commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,65 @@
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards, UseInterceptors } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthGuard } from 'src/guards/auth.guards';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+    constructor(private usersService: UsersService, private authService: AuthService) { }
+
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoami(@CurrentUser() user: User) {
+        return user;
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
+
+    @Post("/signup")
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post("/signin")
+    async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get("/:id")
+    async getUser(@Param("id") id: string) {
+        const user = await this.usersService.findOne(parseInt(id));
+        if (!user) {
+            throw new NotFoundException("user not found");
+        }
+        return user;
+    }
+
+    @Get("/")
+    getUsersByEmail(@Query("email") email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Patch("/:id")
+    updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
+    }
+
+    @Delete("/:id")
+    deleteUser(@Param("id") id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
+}
